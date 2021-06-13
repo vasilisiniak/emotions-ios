d4bfd113f145cfdebffbd3f3c258bb48a5482104
vasili siniak <vasili.siniak@gmail.com>
2021-06-13 18:56:42 +0300

    Fix layout issue after opening sharing menu

--- iOSViewControllers/iOSViewControllers/EmotionsGroupsViewController+Menu.swift
+++ iOSViewControllers/iOSViewControllers/EmotionsGroupsViewController+Menu.swift
@@ -25,13 +25,24 @@ extension EmotionsGroupsViewController {
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
+
+        // MARK: - UIViewController
+
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+
+            label.frame = CGRect(x: 0, y: 0, width: width, height: .greatestFiniteMagnitude)
+            label.sizeToPaddedFit()
+        }
         
         // MARK: - Private
         
-        private func createLabel(text: String, width: CGFloat) -> UILabel {
+        private func createLabel(text: String) -> PaddedLabel {
             let label = PaddedLabel()
             label.textInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
             label.text = text
+            label.adjustsFontForContentSizeCategory = true
+            label.font = .preferredFont(forTextStyle: .body)
             label.numberOfLines = 0
             label.frame = CGRect(x: 0, y: 0, width: width, height: .greatestFiniteMagnitude)
             label.sizeToPaddedFit()
@@ -40,15 +51,18 @@ extension EmotionsGroupsViewController {
         }
 
         private let handler: Handler
+        private let width: CGFloat
+        private var label: PaddedLabel!
         
         // MARK: - Internal
 
         typealias Handler = () -> Void
         
         init(text: String, width: CGFloat, handler: @escaping Handler) {
+            self.width = width
             self.handler = handler
             super.init(nibName: nil, bundle: nil)
-            let label = createLabel(text: text, width: width)
+            label = createLabel(text: text)
             view = label
             preferredContentSize = label.frame.size
         }
