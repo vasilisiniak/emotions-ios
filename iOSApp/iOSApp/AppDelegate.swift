e9ec05c4ab8cc7f779fadbab7c324998d7bb6408
vasili siniak <vasili.siniak@gmail.com>
2022-04-05 17:04:29 +0300

    Handle appearance change

--- iOSApp/iOSApp/AppDelegate.swift
+++ iOSApp/iOSApp/AppDelegate.swift
@@ -6,19 +6,31 @@ import Model
     // MARK: - Private
     
     private let compositionRoot = CompositionRoot()
+    private var token: AnyObject?
+
+    func updateAppearance() {
+        window?.overrideUserInterfaceStyle = AppGroup.settings.appearance
+    }
 
     // MARK: - Internal
 
-    var window: UIWindow?
+    var window: UIWindow? {
+        didSet { updateAppearance() }
+    }
 }
 
 extension AppDelegate: UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
         compositionRoot.metricsManager.start()
         compositionRoot.migrationManager.migrate()
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = compositionRoot.emotionsViewController
         window?.makeKeyAndVisible()
+
+        token = AppGroup.settings.add { [weak self] _ in self?.updateAppearance() }
+
         return true
     }
 
