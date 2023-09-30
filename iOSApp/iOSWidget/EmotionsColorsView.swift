ed595f3e2964c09aa2b3411c40163af2b5f345c5
vasili siniak <vasili.siniak@gmail.com>
2023-09-30 15:41:21 +0300

    Adopt iOS 17 SDK for widget

--- iOSApp/iOSWidget/EmotionsColorsView.swift
+++ iOSApp/iOSWidget/EmotionsColorsView.swift
@@ -5,21 +5,38 @@ struct EmotionsColorsView : View {
 
     var body: some View {
         if entry.colors.count > 1 {
-            Rectangle().fill(LinearGradient(
-                gradient: Gradient(colors: entry.colors.map(Color.init)),
-                startPoint: .leading,
-                endPoint: .trailing
-            ))
-        }
-        else {
-            ZStack {
-                Rectangle().fill(LinearGradient(
-                    gradient: Gradient(colors: WidgetProvider.snapshotEntry.colors.map(Color.init)),
+            Rectangle()
+                .fill(LinearGradient(
+                    gradient: Gradient(colors: entry.colors.map(Color.init)),
                     startPoint: .leading,
                     endPoint: .trailing
                 ))
+                .widgetBackground(Color.clear)
+        }
+        else {
+            ZStack {
+                Rectangle()
+                    .fill(LinearGradient(
+                        gradient: Gradient(colors: WidgetProvider.snapshotEntry.colors.map(Color.init)),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    ))
                 Text("Недостаточно записей событий").multilineTextAlignment(.center)
             }
+            .widgetBackground(Color.clear)
+        }
+    }
+}
+
+extension View {
+    func widgetBackground(_ backgroundView: some View) -> some View {
+        if #available(iOSApplicationExtension 17.0, *) {
+            return containerBackground(for: .widget) {
+                backgroundView
+            }
+        }
+        else {
+            return background(backgroundView)
         }
     }
 }
--- iOSApp/iOSWidget/iOSWidget.swift
+++ iOSApp/iOSWidget/iOSWidget.swift
@@ -5,12 +5,19 @@ import SwiftUI
     let kind: String = "iOSWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: WidgetProvider()) { entry in
+        let res = StaticConfiguration(kind: kind, provider: WidgetProvider()) { entry in
             EmotionsColorsView(entry: entry)
         }
         .configurationDisplayName("Карта эмоций")
         .description("Отображает цветовую карту эмоций, вызывнных событиями из дневника")
         .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+
+        if #available(iOSApplicationExtension 15.0, *) {
+            return res.contentMarginsDisabled()
+        }
+        else {
+            return res
+        }
     }
 }
 
